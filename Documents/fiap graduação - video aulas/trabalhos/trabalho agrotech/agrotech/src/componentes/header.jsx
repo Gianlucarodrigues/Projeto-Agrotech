9982bdb61412cfadd49bf75332c6cebfb03ce0b2
fabio <binhojunior2006@gmail.com>
2025-08-29 10:58:45 -0300

    organização de um projeto react, precisa da mudança das rotas e importaões

---
+++ Documents/fiap graduação - video aulas/trabalhos/trabalho agrotech/agrotech/src/componentes/header.jsx
@@ -0,0 +1,23 @@
+import "./header.css";
+import Logo from "../assets/logo.png";
+function Header() {
+  return (
+      <header>
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <nav className="menu">
+          <ul>
+            <li className="sobre"><a href="sobre.html">Sobre</a></li>
+            <li className="sobre"><a href="como-funciona.html">Como funciona</a></li>
+            <li className="sobre"><a href="contatos.html">Contatos</a></li>
+          </ul>
+        </nav>
+        <a href="login.html">
+          <button className="btn-entrar">Entrar</button>    
+        </a>
+      </header>
+  );
+}
+
+export default Header;
